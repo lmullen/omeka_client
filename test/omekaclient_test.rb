5f100c0905d7e59b0c166b3347f596a6892753af
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-13 10:10:04 -0400

    Added test for API key

--- test/omekaclient_test.rb
+++ test/omekaclient_test.rb
@@ -2,18 +2,25 @@
 require "minitest/autorun"
 
 test_endpoint = "http://localhost/omeka-2.1-rc1/api"
+test_api_key = "c56c8f542bc98483b71896523d4faa6321de193b"
 
 require "omeka_client"
 
 describe OmekaClient::Client do
 
   it "must have an endpoint" do
-    OmekaClient::Client.new(test_endpoint).endpoint.must_equal \
-    "http://localhost/omeka-2.1-rc1/api"
+    client = OmekaClient::Client.new(test_endpoint)
+    client.endpoint.must_equal test_endpoint
   end
 
   it "can be created without an API key" do
-    OmekaClient::Client.new(test_endpoint).api_key.must_be_nil
+    client = OmekaClient::Client.new(test_endpoint)
+    client.api_key.must_be_nil
+  end
+
+  it "can be created with an API key" do
+    client = OmekaClient::Client.new(test_endpoint, test_api_key)
+    client.api_key.must_equal test_api_key
   end
 
   it "must successfully request items and collections " do
