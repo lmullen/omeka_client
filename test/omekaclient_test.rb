60979f47fc18001fda1c9862d49cfbd12b8d69c7
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-13 13:59:27 -0400

    DRY in the tests, of all places

--- test/omekaclient_test.rb
+++ test/omekaclient_test.rb
@@ -1,30 +1,28 @@
-# require "minitest/spec"
 require "minitest/autorun"
+require "omeka_client"
 
+# Set up an test client
 test_endpoint = "http://localhost/omeka-2.1-rc1/api"
 test_api_key = "c56c8f542bc98483b71896523d4faa6321de193b"
-
-require "omeka_client"
+resources = ["items", "collections"]
+client = OmekaClient::Client.new(test_endpoint, test_api_key)
 
 describe OmekaClient::Client do
 
   it "must have an endpoint" do
-    client = OmekaClient::Client.new(test_endpoint)
     client.endpoint.must_equal test_endpoint
   end
 
   it "can be created without an API key" do
-    client = OmekaClient::Client.new(test_endpoint)
-    client.api_key.must_be_nil
+    keyless = OmekaClient::Client.new(test_endpoint)
+    keyless.api_key.must_be_nil
   end
 
   it "can be created with an API key" do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
     client.api_key.must_equal test_api_key
   end
 
   it "must successfully request items and collections " do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
     resources = ["items", "collections"]
     resources.each do |resource|
       client.get(resource).code.must_equal 200
@@ -32,16 +30,12 @@ describe OmekaClient::Client do
   end
 
   it "must successfully request a single item or collection" do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
-    resources = ["items", "collections"]
     resources.each do |resource|
       client.get(resource, 1).code.must_equal 200
     end
   end
 
   it "must return a persistent wrapper for a GET request" do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
-    resources = ["items", "collections"]
     resources.each do |resource|
       client.get(resource).must_be_instance_of \
       Rest::Wrappers::NetHttpPersistentResponseWrapper
@@ -49,32 +43,26 @@ describe OmekaClient::Client do
   end
 
   it "must return a hash or array for a GET request" do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
-    resources = ["items", "collections"]
     resources.each do |resource|
       client.get_hash(resource).must_be_instance_of Array || Hash
     end
   end
 
   it "must return a representation of the site description" do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
     client.site['title'].wont_be_nil
   end
 
   it "must list the resources available via the API" do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
     client.resources.must_be_instance_of Hash
     client.resources.wont_be_empty
   end
 
   it "must list the items available via the API" do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
     client.items.must_be_instance_of Array
     client.items.wont_be_empty
   end
 
   it "must list the collections available via the API" do
-    client = OmekaClient::Client.new(test_endpoint, test_api_key)
     client.collections.must_be_instance_of Array
     client.collections.wont_be_empty
   end
