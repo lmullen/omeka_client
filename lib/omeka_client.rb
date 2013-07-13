564364bf61d3dfac442c583f03c07d5ebaec88d9
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-13 02:38:54 -0400

    Constructor method, and return site information

--- lib/omeka_client.rb
+++ lib/omeka_client.rb
@@ -1,5 +1,21 @@
+require "rest"
+require "json"
 require "omeka_client/version"
 
-module OmekaClient
-  # Your code goes here...
+class OmekaClient
+
+  attr_accessor :endpoint, :key, :rest
+
+  def initialize(endpoint, key)
+    @endpoint = endpoint
+    @key = key
+    @rest = Rest::Client.new
+  end
+
+  def site
+    result =  self.rest.get(self.endpoint + "api/site")
+    # puts result.class
+    JSON.parse(result.body)
+  end
+
 end
--- lib/omeka_client/version.rb
+++ lib/omeka_client/version.rb
@@ -1,3 +1,3 @@
-module OmekaClient
+class OmekaClient
   VERSION = "0.0.1"
 end
