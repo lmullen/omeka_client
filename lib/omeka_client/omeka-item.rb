35b06706c6f9f98d2804d24601c5e75d5013ce8c
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-14 23:21:58 -0400

    Added fields to OmekaItem, especially Dublin Core

--- lib/omeka_client/omeka-item.rb
+++ lib/omeka_client/omeka-item.rb
@@ -1,5 +1,6 @@
-module OmekaClient
+require "ostruct"
 
+module OmekaClient
 
   # 
   # A class to represent an item in an Omeka site
@@ -14,14 +15,46 @@ module OmekaClient
     :item_type, :collection, :owner, :files, :tags, :dublin_core, \
     :item_type_metadata, :extended_resources
 
-
     # 
     # Parse the data we got from the API into handy methods
-    # @param  hash [Hash] The hash returned from OmekaClient::Client::get_hash
+    # @param  hash [Hash] Uses the hash from OmekaClient::Client::get_hash
     # 
     def initialize(hash)
 
+      # Some of these values have strings. Others return arrays or hashes.
       @id = hash['id']
+      @url = hash['url']
+      @public = hash['public']
+      @featured = hash['featured']
+      @added = hash['added']
+      @modified = hash['modified']
+      @item_type = hash['item_type']
+      @collection = hash['collection']
+      @owner = hash['owner']
+      @files = hash['files']
+      @tags = hash['tags']
+      @extended_resources = ['extended_resources']
+
+      # OpenStruct.new requires a hash of method names and methods values,
+      # which we construct here for Dublin Core and for the Item Type
+      # Metadata. The downside is that we are discarding some data.
+      # Element names become method names: "Lesson Plan" to "lesson_plan"
+      dc_metadata = Hash.new
+      item_metadata = Hash.new
+      hash['element_texts'].each do |e|
+        if e['element_set']['name'] == "Dublin Core"
+          method_name = e['element']['name'].downcase.gsub(/\s/, '_')
+          dc_metadata[method_name] = e['text']
+        elsif e['element_set']['name'] == "Item Type Metadata"
+          method_name = e['element']['name'].downcase.gsub(/\s/, '_')
+          item_metadata[method_name] = e['text']
+        end
+      end
+
+      # The OpenStruct will provide methods of the style
+      # item.dublin_core.title
+      @dublin_core = OpenStruct.new(dc_metadata)
+      @item_type_metadata = OpenStruct.new(item_metadata)
 
     end
 
--- test/omeka-item_test.rb
+++ test/omeka-item_test.rb
@@ -12,8 +12,67 @@ item_single = client.omeka_items(1)
 
 describe OmekaClient::OmekaItem do
 
-  it "should have an id" do
+  it "should have an ID" do
     item_single.id.must_equal 1
+  end
+
+  it "should have a URL" do
+    item_single.url.must_be_instance_of String
+  end
+
+  it "should have a URL" do
+    item_single.url.must_be_instance_of String
+  end
+
+  it "should know whether it's public" do
+    item_single.public.wont_be_nil
+  end
+
+  it "should know whether it's featured" do
+    item_single.featured.wont_be_nil
+  end
+
+  it "should have a date added" do
+    item_single.added.must_be_instance_of String
+  end
+
+  it "should have a date modified" do
+    item_single.modified.must_be_instance_of String
+  end
+
+  it "should have an item type" do
+    item_single.item_type.must_be_instance_of Hash
+  end
+
+  it "should have a collection" do
+    item_single.collection.must_be_instance_of Hash
+  end
+
+  it "should have files" do
+    item_single.files.must_be_instance_of Hash
+  end
+
+  it "should have tags" do
+    item_single.tags.must_be_instance_of Array
+  end
+
+  it "should have extended resources" do
+    item_single.extended_resources.must_be_instance_of Array
+  end
+
+  it "should have the Dublin Core metadata" do
+    item_single.dublin_core.must_be_instance_of OpenStruct
+    dc_fields = [:title, :subject, :contributor, :description, :creator, \
+    :source, :publisher,  :date,  :rights, :relation, :format, :language, \
+    :type, :identifier, :coverage]
+    dc_fields.each do |field|
+      item_single.dublin_core.send(field).must_be_instance_of String
+    end
+  end
+
+  it "should have the Item Type metadata" do
+    item_single.item_type_metadata.must_be_instance_of OpenStruct
+    # The item type is unpredictable, so it's hard to be more specific
   end
 
 end
