350252c660fc8249a7ab888305df789ec10c33ea
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-31 18:02:10 -0400

    Added dublin_core and item_type_metadata as instance variables

--- lib/omeka_client/omeka-item.rb
+++ lib/omeka_client/omeka-item.rb
@@ -11,14 +11,34 @@ module OmekaClient
   class OmekaItem
 
     # Instance variables for each of the main parts of the metadata
-    attr_accessor :data
+    attr_accessor :data, :dublin_core, :item_type_metadata
 
     #
     # Parse the data we got from the API into handy methods
     # @param  hash [Hash] Uses the hash from OmekaClient::Client::get_hash
     #
     def initialize(hash)
-      @data = RecursiveOpenStruct.new(hash)
+      @data = RecursiveOpenStruct.new(hash, :recurse_over_arrays => true)
+
+      # @data.new_ostruct_member(:dublin_core)
+
+      dublin_core = Hash.new
+      item_type_metadata = Hash.new
+      @data.element_texts.each do |element_text|
+        if element_text.element_set.name == "Dublin Core"
+          method_name = element_text.element.name.downcase.gsub(/\s/, '_')
+          value       = element_text.text
+          dublin_core[method_name] = value
+        elsif element_text.element_set.name == "Item Type Metadata"
+          method_name = element_text.element.name.downcase.gsub(/\s/, '_')
+          value       = element_text.text
+          item_type_metadata[method_name] = value
+        end
+      end
+
+      @dublin_core = RecursiveOpenStruct.new(dublin_core)
+      @item_type_metadata = RecursiveOpenStruct.new(item_type_metadata)
+
     end
 
   end
