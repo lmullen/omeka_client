2679956a42de778c05a94cba0b4119682c0c1c14
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-08-07 17:24:07 -0400

    Abstract API requests into a private helper method
    The method build_request now handles all the logic for building urls and query
    parameters, and the default post, put, get, and delete methods now call it.

--- lib/omeka_client/client.rb
+++ lib/omeka_client/client.rb
@@ -10,14 +10,19 @@ module OmekaClient
     attr_accessor :endpoint, :api_key, :connection
 
     # Sets up a new client to interact with an Omeka site
+    #
     # @param  endpoint [String] the URL of the Omeka API endpoint, without a
     #   trailing slash. For example: "{http://localhost/omeka/api}"
     # @param  api_key [String] The API key of the Omeka user. This can
     #   be null for GET requests,  but  is required for POST, PUT, and DELETE
     #   requests.
+    # 
     # @return [OmekaClient] The attribute @connection is the client itself,
     #   which is an instance from the Rest gem. This client can be used to
     #   perform arbitrary REST queries. See https://github.com/iron-io/rest
+    #
+    # @since 0.0.1
+    #
     def initialize(endpoint, api_key = nil )
       @endpoint = endpoint
       @api_key = api_key
@@ -37,10 +42,7 @@ module OmekaClient
     # @return [NetHttpPersistentResponseWrapper] A wrapper around the object
     # @since 0.0.1
     def get(resource, id = nil, query = {} )
-      url = self.endpoint + "/" + resource
-      url += "/" + id.to_s unless id.nil?
-      query[:key] = self.api_key unless self.api_key.nil?
-      self.connection.get(url, :params => query)
+      build_request("get", resource, id, query)
     end
 
     # Parse a GET request into a useable format
@@ -69,38 +71,21 @@ module OmekaClient
     # @return [NetHttpPersistentResponseWrapper] A wrapper around the object
     # @since 0.0.3
     def post(resource, body = nil, query = {} )
-      url = self.endpoint + "/" + resource
-      query['key'] = self.api_key unless self.api_key.nil?
-      self.connection.post(url, :body => body, :params => query)
+      build_request("post", resource, nil, body, query)
     end
 
     # Generic DELETE request to the Omeka site
     # @param  resource [String] The type of Omeka resource to delete, e.g.
     #   "items", "collections"
     # @param  id [Integer] The id number of the Omeka resource to delete
-    # @param  query = {} [Hash] Additional query parameters
     #
     # @return [NetHttpPersistentResponseWrapper] A wrapper around the object
     # @since 0.0.3
-    def delete(resource, id, query = {} )
-      url = self.endpoint + "/" + resource
-      url += "/" + id.to_s unless id.nil?
-      query[:key] = self.api_key unless self.api_key.nil?
-
-      # The rest gem that provides out functionality has a bug. The Omeka API
-      # returns 204 No Content on DELETE, indicating that the item has been
-      # successfully deleted but that there is no body to return. The rest
-      # gem assumes there will be a body, so it throws a type error. Until
-      # this is fixed, we just rescue the error and don't worry about it.
-      begin
-        self.connection.delete(url, :params => query)
-      rescue TypeError
-        # Not putting the error to stdout
-      end
-
+    def delete(resource, id)
+      build_request("delete", resource, id, nil, {})
     end
 
-    # Generic DELETE request to the Omeka site
+    # Generic PUT request to the Omeka site
     # @param  resource [String] The type of Omeka resource to update, e.g.
     #   "items", "collections"
     # @param  id [Integer] The id number of the Omeka resource to update
@@ -109,10 +94,7 @@ module OmekaClient
     # @return [NetHttpPersistentResponseWrapper] A wrapper around the object
     # @since 0.0.3
     def put(resource, id, body, query = {} )
-      url = self.endpoint + "/" + resource
-      url += "/" + id.to_s unless id.nil?
-      query[:key] = self.api_key unless self.api_key.nil?
-      self.connection.put(url, :body => body, :params => query)
+      build_request("put", resource, id, body, query)
     end
 
     # Methods that use classes
@@ -225,13 +207,46 @@ module OmekaClient
       self.get_hash('collections')
     end
 
-    # Other generic methods to write
-    # TODO: tags
-    # TODO: files
-    # TODO: item_types
-    # TODO: elements
-    # TODO: element_sets
-    # TODO: users
+    # Helper method to build an API request
+    #
+    # @param method [String] The type of REST request to make: "get", "post", 
+    #   "put", or "delete".
+    # @param resource [String] The type of resource to request from the Omeka 
+    #   site, e.g., "items" or "site".
+    # @param id [Integer] The id of the resource to request from the Omeka 
+    #   site.
+    # @param body [] The body of a request in a PUT or POST request.
+    # @param query [Hash] Additional query parameters for the request.
+    #
+    # @return [NetHttpPersistentResponseWrapper] A wrapper around the API's 
+    # response, containing the HTTP code and the response body.
+    # 
+    # @since 1.0.0
+    #
+    def build_request(method, resource = nil, id = nil, body =nil, query = {})
+
+      url =  self.endpoint 
+      url += "/" + resource unless resource.nil?
+      url += "/" + id.to_s unless id.nil?
+      query[:key] = self.api_key unless self.api_key.nil?
+
+      case method
+      when "get"
+        self.connection.get(url, :params => query)
+      when "post"
+        self.connection.post(url, :body => body, :params => query)
+      when "put"
+        self.connection.put(url, :body => body, :params => query)
+      when "delete"
+        begin
+          self.connection.delete(url, :params => query)
+        rescue TypeError
+          # Not putting the error to stdout
+        end
+      end
+
+    end
+    private :build_request
 
   end
 
