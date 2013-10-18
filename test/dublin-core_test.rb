6e41a93fa8be1fde7688238296bd62dbcf6e8bb9
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-10-17 22:08:52 -0400

    Fixed old method name in test

--- test/dublin-core_test.rb
+++ test/dublin-core_test.rb
@@ -5,8 +5,8 @@ require "omeka_client"
 test_endpoint = "http://localhost/omeka-2.1-rc1/api"
 test_api_key  = "3b036221e180af46bafa4b5e4a1db30e84e78e89"    # contributor
 client = OmekaClient::Client.new(test_endpoint, test_api_key)
-item_a = client.omeka_items(1)
-item_b = client.omeka_items(2)
+item_a = client.get_item(1)
+item_b = client.get_item(2)
 
 describe OmekaClient::DublinCore do
   it "must use singleton methods so that items maintain their own data" do
